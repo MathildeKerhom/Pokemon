d45a4b1082f5375ee7bf462ed7a9df36aee9114d
Florian Jarnoin <florian.jarnoin@orange.fr>
2015-12-10 17:02:30 +0100

    adding topBar panel

---
+++ PokemonGarden/PokemonGarden/Classes/TopBarContent.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace PokemonGarden
+{
+	public class TopBarContent
+	{
+		private int pokemonTotal;
+		private int pokemonActual;
+		private int seedActual;
+		private int goldActual;
+		private int rewardTotal;
+		private int rewardActual;
+
+		public TopBarContent()
+		{
+			this.pokemonTotal = 0;
+			this.pokemonActual = 0;
+			this.seedActual = 0;
+			this.goldActual = 0;
+			this.rewardTotal = 0;
+			this.rewardActual = 0;
+		}
+
+		public int PokemonTotal
+		{
+			get
+			{
+				return pokemonTotal;
+			}
+
+			set
+			{
+				this.pokemonTotal = value;
+			}
+		}
+
+		public int PokemonActual
+		{
+			get
+			{
+				return pokemonActual;
+			}
+
+			set
+			{
+				this.pokemonActual = value;
+			}
+		}
+
+		public int SeedActual
+		{
+			get
+			{
+				return seedActual;
+			}
+
+			set
+			{
+				this.seedActual = value;
+			}
+		}
+
+		public int GoldActual
+		{
+			get
+			{
+				return goldActual;
+			}
+
+			set
+			{
+				this.goldActual = value;
+			}
+		}
+
+		public int RewardTotal
+		{
+			get
+			{
+				return rewardTotal;
+			}
+
+			set
+			{
+				this.rewardTotal = value;
+			}
+		}
+
+		public int RewardActual
+		{
+			get
+			{
+				return rewardActual;
+			}
+
+			set
+			{
+				this.rewardActual = value;
+			}
+		}
+	}
+}
--- PokemonGarden/PokemonGarden/MainPage.xaml.cs
+++ PokemonGarden/PokemonGarden/MainPage.xaml.cs
@@ -17,14 +17,16 @@ using Windows.UI.Xaml.Navigation;
 
 namespace PokemonGarden
 {
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public sealed partial class MainPage:Page
+	{
+		public MainPage()
+		{
+			this.InitializeComponent();
+			this.topBar.GoldActual += 10;
+			this.topBar.GoldActual += 100;
+		}
+	}
 }
