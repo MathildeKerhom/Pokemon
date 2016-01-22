f1ce7f2fb910a20d6ae39cdda86117971739067e
Florian Jarnoin <florian.jarnoin@outlook.fr>
2016-01-22 22:29:44 +0100

    add player picture on village, add dynamic resize of topBar under 800 px(windowsWidth), modify garden grid with dragNDrop

--- PokemonGarden/PokemonGarden/View/Garden.xaml.cs
+++ PokemonGarden/PokemonGarden/View/Garden.xaml.cs
@@ -28,5 +28,105 @@ namespace PokemonGarden.View
 			this.InitializeComponent();
 			this.DataContext = Player.GetPlayer();
 		}
+
+		private void seed_DragEnter(object sender, DragEventArgs e)
+		{
+			e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Move;
+		}
+
+		private void seed_DragLeave(object sender, DragEventArgs e)
+		{
+			e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.None;
+		}
+
+		private void seed1_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed1.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed2_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed2.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed3_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed3.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed4_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed4.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed5_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed5.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed6_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed6.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed7_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed7.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed8_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed8.DataContext = (MarketSeed)seed;
+			}
+		}
+
+		private void seed9_Drop(object sender, DragEventArgs e)
+		{
+			object seed;
+			e.Data.Properties.TryGetValue("seedSource", out seed);
+			if (seed != null)
+			{
+				seed9.DataContext = (MarketSeed)seed;
+			}
+		}
 	}
 }
