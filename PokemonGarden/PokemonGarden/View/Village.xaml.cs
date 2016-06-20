bb6a6effd92edd71ec91c57ee0ec9b15066f5d2c
Florian Jarnoin <florian.jarnoin@outlook.fr>
2016-06-20 20:18:02 +0200

    update homepage view to have mvvm architectural pattern

--- PokemonGarden/PokemonGarden/View/Village.xaml.cs
+++ PokemonGarden/PokemonGarden/View/Village.xaml.cs
@@ -1,18 +1,7 @@
-﻿using PokemonGarden.View;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using PokemonGarden.ViewModel;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -21,100 +10,108 @@ namespace PokemonGarden
 	/// <summary>
 	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
 	/// </summary>
-	public sealed partial class Village:Page
+	public sealed partial class Village : Page
 	{
-        public Village()
-        {
-            this.InitializeComponent();
-            // set keyDown event to currentWindow
-            Window.Current.Content.KeyDown += Content_KeyDown;
-        }
+		private int xPos, yPos, xMin, xMax, yMin, yMax;
 
-        private void Content_KeyDown(object sender, KeyRoutedEventArgs e)
-        {
-            switch (e.Key)
-            {
-                case Windows.System.VirtualKey.Left:
-                case Windows.System.VirtualKey.Q:
-                    if (Grid.GetColumn(this.me) != 0)
-                    {
-                        Grid.SetColumn(this.me, Grid.GetColumn(this.me) - 1);
-                    }
-                    break;
-                case Windows.System.VirtualKey.Right:
-                case Windows.System.VirtualKey.D:
-                    if (Grid.GetColumn(this.me) != 9)
-                    {
-                        Grid.SetColumn(this.me, Grid.GetColumn(this.me) + 1);
-                    }
-                    break;
-                case Windows.System.VirtualKey.Up:
-                case Windows.System.VirtualKey.Z:
-                    if (Grid.GetRow(this.me) != 0)
-                    {
-                        Grid.SetRow(this.me, Grid.GetRow(this.me) - 1);
-                    }
-                    break;
-                case Windows.System.VirtualKey.Down:
-                case Windows.System.VirtualKey.S:
-                    if (Grid.GetRow(this.me) != 9)
-                    {
-                        Grid.SetRow(this.me, Grid.GetRow(this.me) + 1);
-                    }
-                    break;
-            }
+		public int XPos
+		{
+			get
+			{
+				return this.xPos;
+			}
+		}
 
-            if ((Grid.GetColumn(this.me) == 0 || Grid.GetColumn(this.me) == 1)
-                && (Grid.GetRow(this.me) == 0 || Grid.GetRow(this.me) == 1))
-            {
-                (Window.Current.Content as Frame).Navigate(typeof(Market));
-            }
+		public int YPos
+		{
+			get
+			{
+				return this.yPos;
+			}
+		}
 
-            else if (((Grid.GetColumn(this.me) == 3 || Grid.GetColumn(this.me) == 5)
-                    && (Grid.GetRow(this.me) == 2 ||
-                        Grid.GetRow(this.me) == 3 ||
-                        Grid.GetRow(this.me) == 4))
-                || (Grid.GetColumn(this.me) == 4
-                    && (Grid.GetRow(this.me) == 1 ||
-                        Grid.GetRow(this.me) == 2 ||
-                        Grid.GetRow(this.me) == 3 ||
-                        Grid.GetRow(this.me) == 4)))
-            {
-                (Window.Current.Content as Frame).Navigate(typeof(Garden));
-            }
+		public int XMin
+		{
+			get
+			{
+				return xMin;
+			}
+		}
 
-            else if ((Grid.GetColumn(this.me) == 8
-                    && (Grid.GetRow(this.me) == 4 ||
-                        Grid.GetRow(this.me) == 5))
-                 || (Grid.GetColumn(this.me) == 9
-                    && Grid.GetRow(this.me) == 5))
-            {
-                (Window.Current.Content as Frame).Navigate(typeof(Laboratory));
-            }
+		public int XMax
+		{
+			get
+			{
+				return xMax;
+			}
+		}
 
-            else if (Grid.GetColumn(this.me) == 2 && Grid.GetRow(this.me) == 8)
-            {
-                (Window.Current.Content as Frame).Navigate(typeof(Arena));
-            }
+		public int YMin
+		{
+			get
+			{
+				return yMin;
+			}
+		}
 
-            else
-            {
-                return;
-            }
+		public int YMax
+		{
+			get
+			{
+				return yMax;
+			}
+		}
 
-            // unset keyDown event from currentWindow to avoid allowing
-            //directional click on Window where Navigate arrives into
-            Window.Current.Content.KeyDown -= Content_KeyDown;
-        }
+		public Village()
+		{
+			this.InitializeComponent();
+			new VillageViewModel(this);
+		}
 
-        private void market_Click(object sender, RoutedEventArgs e)
-        {
+		/// <summary>
+		/// initialize caracter position
+		/// </summary>
+		/// <param name="xPos"></param>
+		/// <param name="yPos"></param>
+		public void Init(int xPos, int yPos, int xMin, int xMax, int yMin, int yMax)
+		{
+			this.xPos = xPos;
+			this.yPos = yPos;
+			this.xMin = xMin;
+			this.xMax = xMax;
+			this.yMin = yMin;
+			this.yMax = yMax;
+			Grid.SetColumn(this.me, xPos);
+			Grid.SetRow(this.me, yPos);
+		}
 
-        }
+		/// <summary>
+		/// move the img caracter
+		/// </summary>
+		/// <param name="xPos"></param>
+		/// <param name="yPos"></param>
+		public void MoveCaracterTo(int xPos, int yPos)
+		{
+			if (this.xPos != xPos && xPos >= this.xMin && xPos <= this.xMax)
+			{
+				Grid.SetColumn(this.me, xPos);
+				this.xPos = xPos;
+			}
+			if (this.yPos != yPos && yPos >= this.xMin && yPos <= this.xMax)
+			{
+				Grid.SetRow(this.me, yPos);
+				this.yPos = yPos;
+			}
+		}
 
-        private void market_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            (Window.Current.Content as Frame).Navigate(typeof(Garden));
-        }
-    }
+		public void OnKeyDown(KeyEventHandler onKeyDown)
+		{
+			Window.Current.Content.KeyDown += onKeyDown;
+		}
+
+		public void UnMapKeyDown(KeyEventHandler onKeyDown)
+		{
+			Window.Current.Content.KeyDown -= onKeyDown;
+		}
+	}
 }
---
+++ PokemonGarden/PokemonGarden/ViewModel/VillageViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonGarden.View;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace PokemonGarden.ViewModel
+{
+	public class VillageViewModel
+	{
+		Village village;
+
+		public VillageViewModel(Village village)
+		{
+			this.village = village;
+			this.village.Init(5, 6, 0, 9, 0, 9);
+			// set keyDown event to currentWindow
+			this.village.OnKeyDown(Content_KeyDown);
+		}
+
+		private void Content_KeyDown(object sender, KeyRoutedEventArgs e)
+		{
+			switch (e.Key)
+			{
+				case Windows.System.VirtualKey.Left:
+				case Windows.System.VirtualKey.Q:
+					this.village.MoveCaracterTo(this.village.XPos - 1, this.village.YPos);
+					break;
+				case Windows.System.VirtualKey.Right:
+				case Windows.System.VirtualKey.D:
+					this.village.MoveCaracterTo(this.village.XPos + 1, this.village.YPos);
+					break;
+				case Windows.System.VirtualKey.Up:
+				case Windows.System.VirtualKey.Z:
+					this.village.MoveCaracterTo(this.village.XPos, this.village.YPos - 1);
+					break;
+				case Windows.System.VirtualKey.Down:
+				case Windows.System.VirtualKey.S:
+					this.village.MoveCaracterTo(this.village.XPos, this.village.YPos + 1);
+					break;
+			}
+
+			executNavigate();
+		}
+
+		private void executNavigate()
+		{
+			if (this.village.XPos <= 1 && this.village.YPos <= 1)
+			{
+				navigateTo(typeof(Market));
+			}
+			else if (this.village.XPos >= 3 && this.village.XPos <= 5 && this.village.YPos >= 2 && this.village.YPos <= 4)
+			{
+				navigateTo(typeof(Garden));
+			}
+			else if ((this.village.XPos == 8 && (this.village.YPos == 4 || this.village.YPos == 5)) || (this.village.XPos == 9 && this.village.YPos == 5))
+			{
+				navigateTo(typeof(Laboratory));
+			}
+			else if (this.village.XPos == 2 && this.village.YPos == 8)
+			{
+				navigateTo(typeof(Arena));
+			}
+		}
+
+		private void market_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void market_Tapped(object sender, TappedRoutedEventArgs e)
+		{
+			navigateTo(typeof(Garden));
+		}
+
+		private void navigateTo(Type type)
+		{
+			this.village.UnMapKeyDown(Content_KeyDown);
+			(Window.Current.Content as Frame).Navigate(type);
+		}
+	}
+}
