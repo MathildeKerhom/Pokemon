832c4437cca06068e2740f470a9919043b31b19a
Florian Jarnoin <florian.jarnoin@orange.fr>
2016-06-21 16:35:57 +0200

    update mvvm model for village view

--- PokemonGarden/PokemonGarden/View/Village.xaml.cs
+++ PokemonGarden/PokemonGarden/View/Village.xaml.cs
@@ -12,106 +12,13 @@ namespace PokemonGarden
 	/// </summary>
 	public sealed partial class Village : Page
 	{
-		private int xPos, yPos, xMin, xMax, yMin, yMax;
-
-		public int XPos
-		{
-			get
-			{
-				return this.xPos;
-			}
-		}
-
-		public int YPos
-		{
-			get
-			{
-				return this.yPos;
-			}
-		}
-
-		public int XMin
-		{
-			get
-			{
-				return xMin;
-			}
-		}
-
-		public int XMax
-		{
-			get
-			{
-				return xMax;
-			}
-		}
-
-		public int YMin
-		{
-			get
-			{
-				return yMin;
-			}
-		}
-
-		public int YMax
-		{
-			get
-			{
-				return yMax;
-			}
-		}
+		public Image Me { get; set; }
 
 		public Village()
 		{
 			this.InitializeComponent();
+			this.Me = this.me;
 			new VillageViewModel(this);
-		}
-
-		/// <summary>
-		/// initialize caracter position
-		/// </summary>
-		/// <param name="xPos"></param>
-		/// <param name="yPos"></param>
-		public void Init(int xPos, int yPos, int xMin, int xMax, int yMin, int yMax)
-		{
-			this.xPos = xPos;
-			this.yPos = yPos;
-			this.xMin = xMin;
-			this.xMax = xMax;
-			this.yMin = yMin;
-			this.yMax = yMax;
-			Grid.SetColumn(this.me, xPos);
-			Grid.SetRow(this.me, yPos);
-		}
-
-		/// <summary>
-		/// move the img caracter
-		/// </summary>
-		/// <param name="xPos"></param>
-		/// <param name="yPos"></param>
-		public void MoveCaracterTo(int xPos, int yPos)
-		{
-			if (this.xPos != xPos && xPos >= this.xMin && xPos <= this.xMax)
-			{
-				Grid.SetColumn(this.me, xPos);
-				this.xPos = xPos;
-			}
-			if (this.yPos != yPos && yPos >= this.xMin && yPos <= this.xMax)
-			{
-				Grid.SetRow(this.me, yPos);
-				this.yPos = yPos;
-			}
-		}
-
-		public void OnKeyDown(KeyEventHandler onKeyDown)
-		{
-			Window.Current.Content.KeyDown += onKeyDown;
-		}
-
-		public void UnMapKeyDown(KeyEventHandler onKeyDown)
-		{
-			Window.Current.Content.KeyDown -= onKeyDown;
 		}
 	}
 }
--- PokemonGarden/PokemonGarden/ViewModel/VillageViewModel.cs
+++ PokemonGarden/PokemonGarden/ViewModel/VillageViewModel.cs
@@ -12,14 +12,31 @@ namespace PokemonGarden.ViewModel
 {
 	public class VillageViewModel
 	{
-		Village village;
+		private int xPos, yPos, xMin, xMax, yMin, yMax;
+		private Village village;
 
 		public VillageViewModel(Village village)
 		{
 			this.village = village;
-			this.village.Init(5, 6, 0, 9, 0, 9);
+			this.init();
 			// set keyDown event to currentWindow
-			this.village.OnKeyDown(Content_KeyDown);
+			Window.Current.Content.KeyDown += Content_KeyDown;
+		}
+
+		/// <summary>
+		/// initialize view
+		/// </summary>
+		private void init()
+		{
+			this.xPos = 5;
+			this.yPos = 6;
+			this.xMin = 0;
+			this.xMax = 9;
+			this.yMin = 0;
+			this.yMax = 9;
+
+			Grid.SetColumn(this.village.Me, this.xPos);
+			Grid.SetRow(this.village.Me, this.yPos);
 		}
 
 		private void Content_KeyDown(object sender, KeyRoutedEventArgs e)
@@ -28,58 +45,74 @@ namespace PokemonGarden.ViewModel
 			{
 				case Windows.System.VirtualKey.Left:
 				case Windows.System.VirtualKey.Q:
-					this.village.MoveCaracterTo(this.village.XPos - 1, this.village.YPos);
+					this.moveCaracterTo(this.xPos - 1, this.yPos);
 					break;
 				case Windows.System.VirtualKey.Right:
 				case Windows.System.VirtualKey.D:
-					this.village.MoveCaracterTo(this.village.XPos + 1, this.village.YPos);
+					this.moveCaracterTo(this.xPos + 1, this.yPos);
 					break;
 				case Windows.System.VirtualKey.Up:
 				case Windows.System.VirtualKey.Z:
-					this.village.MoveCaracterTo(this.village.XPos, this.village.YPos - 1);
+					this.moveCaracterTo(this.xPos, this.yPos - 1);
 					break;
 				case Windows.System.VirtualKey.Down:
 				case Windows.System.VirtualKey.S:
-					this.village.MoveCaracterTo(this.village.XPos, this.village.YPos + 1);
+					this.moveCaracterTo(this.xPos, this.yPos + 1);
 					break;
 			}
 
 			executNavigate();
 		}
 
+		/// <summary>
+		/// navigate to the view when user reach it
+		/// </summary>
 		private void executNavigate()
 		{
-			if (this.village.XPos <= 1 && this.village.YPos <= 1)
+			if (this.xPos <= 1 && this.yPos <= 1)
 			{
 				navigateTo(typeof(Market));
 			}
-			else if (this.village.XPos >= 3 && this.village.XPos <= 5 && this.village.YPos >= 2 && this.village.YPos <= 4)
+			else if (this.xPos >= 3 && this.xPos <= 5 && this.yPos >= 2 && this.yPos <= 4)
 			{
 				navigateTo(typeof(Garden));
 			}
-			else if ((this.village.XPos == 8 && (this.village.YPos == 4 || this.village.YPos == 5)) || (this.village.XPos == 9 && this.village.YPos == 5))
+			else if ((this.xPos == 8 && (this.yPos == 4 || this.yPos == 5)) || (this.xPos == 9 && this.yPos == 5))
 			{
 				navigateTo(typeof(Laboratory));
 			}
-			else if (this.village.XPos == 2 && this.village.YPos == 8)
+			else if (this.xPos == 2 && this.yPos == 8)
 			{
 				navigateTo(typeof(Arena));
 			}
 		}
 
-		private void market_Click(object sender, RoutedEventArgs e)
+		/// <summary>
+		/// move the img caracter
+		/// </summary>
+		/// <param name="xPos"></param>
+		/// <param name="yPos"></param>
+		private void moveCaracterTo(int xPos, int yPos)
 		{
-
+			if (this.xPos != xPos && xPos >= this.xMin && xPos <= this.xMax)
+			{
+				Grid.SetColumn(this.village.Me, xPos);
+				this.xPos = xPos;
+			}
+			if (this.yPos != yPos && yPos >= this.yMin && yPos <= this.yMax)
+			{
+				Grid.SetRow(this.village.Me, yPos);
+				this.yPos = yPos;
+			}
 		}
 
-		private void market_Tapped(object sender, TappedRoutedEventArgs e)
-		{
-			navigateTo(typeof(Garden));
-		}
-
+		/// <summary>
+		/// go to precise view
+		/// </summary>
+		/// <param name="type"></param>
 		private void navigateTo(Type type)
 		{
-			this.village.UnMapKeyDown(Content_KeyDown);
+			Window.Current.Content.KeyDown -= Content_KeyDown;
 			(Window.Current.Content as Frame).Navigate(type);
 		}
 	}
